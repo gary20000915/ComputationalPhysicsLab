52c0cec0de21d7c2d89c752288a355af0e3317c7
Gary <garyphys0915@gapp.nthu.edu.tw>
2022-10-24 17:49:48 +0800

    lecture practice.

--- Lab1/mysolver.py
+++ Lab1/mysolver.py
@@ -1,125 +1,107 @@
-import numpy as np
 """
 
 This program solves Initial Value Problems (IVP).
 We support three numerical meothds: Euler, Rk2, and Rk4
 
-Example Usage:
-
-    def yderive_func(t,y,a,b,c):
-        yderive = np.zeros(len(y))
-        yderive[0] = 0
-        yderive[1] = a, ...
-        return yderive
-
-    y0  = [0,1]
-    t_span = (0,1)
-    t_eval =np.linspace(0,1,100)
-
-    sol = solve_ivp(yderive_func, t_span, y0, 
-                    method="RK4",t_eval=t_eval, args=(K,M))
-
-
-    See `solve_ivp` for detailed description. 
-
 Author: Kuo-Chuan Pan, NTHU 2022.10.06
 For the course, computational physics lab
 
 """
-def solve_ivp(derive_func, t_span, y0, method, t_eval, args):
+
+import numpy as np
+
+def solve_ivp(derive_func, y0, dt, N, method, args):
     """
     Solve Initial Value Problems. 
 
     :param derive_func: a function to describe the derivative of the desired function
-    :param t_span: 2-tuple of floats. the time range to compute the IVP, (t0, tf)
     :param y0: an array. The initial state
+    :param dt: the step time
+    :param N: the number of steps.
     :param method: string. Numerical method to compute. 
                    We support "Euler", "RK2" and "RK4".
-    :param t_eval: array_like. Times at which to store the computed solution, 
-                   must be sorted and lie within t_span.
     :param *args: extra arguments for the derive func.
 
     :return: array_like. solutions. 
-
-    Note: the structe of this function is to mimic the scipy.integrate
-          In the numerical scheme we designed, we didn't check the consistentcy between
-          t_span and t_eval. Be careful. 
-
     """
+    sol_pos, sol_vel = np.array([]), np.array([])
+    for _ in range(N):
+        sol_pos = np.append(sol_pos, y0[0])
+        sol_vel = np.append(sol_vel, y0[1]) 
+        y0 = _update(derive_func,y0, dt, method, *args)
 
-    sol  = np.zeros((len(y0),len(t_eval))) # define the shape of the solution
+    return [sol_pos, sol_vel]
 
-    #
-    # TODO:
-    #
-
-    return sol
-
-def _update(derive_func,y0, dt, t, method, *args):
+def _update(derive_func,y0, dt, method, *args):
     """
     Update the IVP with different numerical method
 
     :param derive_func: the derivative of the function y'
     :param y0: the initial conditions at time t
     :param dt: the time step dt
-    :param t: the time
     :param method: the numerical method
     :param *args: extral parameters for the derive_func
 
-    :return: the next step condition y
+    :return: the next step condition y0
 
     """
 
     if method=="Euler":
-        ynext = _update_euler(derive_func,y0,dt,t,*args)
+        ynext = _update_euler(derive_func,y0,dt,*args)
     elif method=="RK2":
-        ynext = _update_rk2(derive_func,y0,dt,t,*args)
+        ynext = _update_rk2(derive_func,y0,dt,*args)
     elif method=="RK4":
-        ynext = _update_rk4(derive_func,y0,dt,t,*args)
+        ynext = _update_rk4(derive_func,y0,dt,*args)
     else:
         print("Error: mysolve doesn't supput the method",method)
         quit()
     return ynext
 
-def _update_euler(derive_func,y0,dt,t,*args):
+def _update_euler(derive_func,y0,dt,*args):
     """
     Update the IVP with the Euler's method
 
     :return: the next step solution y
 
     """
-
-    #
-    # TODO:
-    #
+    y0 = np.add(y0, derive_func(y0, *args) * dt)
 
     return y0 # <- change here. just a placeholder
 
-def _update_rk2(derive_func,y0,dt,t,*args):
+def _update_rk2(derive_func,y0,dt,*args):
     """
     Update the IVP with the RK2 method
 
     :return: the next step solution y
     """
 
-    #
-    # TODO:
-    #
+    k1 = derive_func(y0,*args)
+    y_temp = y0 + dt * k1 
+    k2 = derive_func(y_temp,*args) 
+    
+    y0 = np.add(y0, (dt / 2) * (k1 + k2))
 
     return y0 # <- change here. just a placeholder
 
-def _update_rk4(derive_func,y0,dt,t,*args):
+def _update_rk4(derive_func,y0,dt,*args):
     """
     Update the IVP with the RK4 method
 
     :return: the next step solution y
     """
 
-    #
-    # TODO:
-    #
+    k1 = derive_func(y0,*args)
+    y_temp = y0 + (dt/2) * k1 # temp for virtual step y*
+    k2 = derive_func(y_temp,*args)
+    y_temp = y0 + (dt/2) * k2
+    k3 = derive_func(y_temp,*args)
+    y_temp = y0 + dt * k3
+    k4 = derive_func(y_temp,*args)
+    
+    y0 = np.add(y0, (1/6) * dt * (k1 + 2*k2 + 2*k3 + k4))
 
     return y0 # <- change here. just a placeholder
+
 
 if __name__=='__main__':
 
@@ -133,39 +115,27 @@ if __name__=='__main__':
     """
 
 
-    def oscillator(t,y,K,M):
-        """
-        The derivate function for an oscillator
-        In this example, we set
+    def oscillator(y,K,M):
+        '''
+        This is the function (osci) defined in the [position, velocity] 
+        and [derivative(position), derivative(velocity)]
+        :param y: [position, velocity]
+        :param k: spring constants
+        :param m: mass constant
+        '''
+        
+        yder =  np.zeros(2)
+        yder[0] = y[1]
+        yder[1] = -y[0] * K/M # the difinition of the acceleration, which is depend on the position.
+        
+        return yder
 
-        y[0] = x
-        y[1] = v
+    K, M  = 1, 1
+    N, t = 100, 20
+    dt = t/N
+    y0 = np.array([1, 0]) # [pos_0, vel_0]
 
-        yderive[0] = x' = v
-        yderive[1] = v' = a
+    sol = solve_ivp(oscillator, y0, dt, N, method="RK4", args=(K,M))
 
-        :param t: the time
-        :param y: the initial condition y
-        :param K: the spring constant
-        :param M: the mass of the oscillator
-
-        """
-
-        #
-        # TODO:
-        #
- 
-        return y # <- change here. just a placeholder
-
-    t_span = (0, 10)
-    y0     = np.array([1,0])
-    t_eval = np.linspace(0,1,100)
-
-    K = 1
-    M = 1
-
-    sol = solve_ivp(oscillator, t_span, y0, 
-                    method="Euler",t_eval=t_eval, args=(K,M))
-
-    print("sol=",sol[0])
+    print("sol=",sol)
     print("Done!")
